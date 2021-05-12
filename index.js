d5dfa3739dcbf51c6160aed7df274d22d4f52ea1
snoor_madih <snoor.madih@gmail.com>
2021-05-12 11:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@
 //   alert('I was clicked!');
 // });
 // }
+const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+//   alert('I was clicked!');
+// });
+// }
